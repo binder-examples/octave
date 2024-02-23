37ac1ff4bb9d4afdab32e444fc50c9672ff75a23
acpennlab <147250307+acpennlab@users.noreply.github.com>
2024-02-23 23:49:20 +0000

    Update and rename .octaverc to startup.m

--- startup.m
+++ startup.m
@@ -1,5 +1,13 @@
+# Warnings to turn off
 warning('off', 'Octave:shadowed-function');
-pkg install -forge statistics statistics-resampling
-pkg load statistics statistics-resampling
-warning('on', 'Octave:shadowed-function');
 warning('off', 'Octave:legacy-function');
+
+# Packages to install
+pkg install -forge statistics statistics-resampling
+
+# Packages to load
+pkg load statistics statistics-resampling
+
+# Warnings to turn back on
+warning('on', 'Octave:shadowed-function');
+
